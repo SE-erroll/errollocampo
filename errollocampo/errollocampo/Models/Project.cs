dfa2eb69ff4bfb7d394b530e046e956726979844
Erroll Ocampo <erroll.ocampo@gmail.com>
2023-10-29 11:31:37 +0800

    Add project files.

---
+++ errollocampo/errollocampo/Models/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace errollocampo.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Technologies { get; set; }
+        public string GitHubLink { get; set; }
+        public string Screenshots { get; set; }
+    }
+}
